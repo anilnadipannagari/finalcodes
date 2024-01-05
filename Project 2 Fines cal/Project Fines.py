32bc60d3520749f66e7ca2827bd758ee465b222b
Nadipannagari Anilkumar <111619322+anilnadipannagari@users.noreply.github.com>
2024-01-05 16:15:04 +0530

    project

---
+++ Project 2 Fines cal/Project Fines.py
@@ -0,0 +1,13 @@
+'''Fine allotment of Student for 
+FOR BOYS
+1.Tie-20
+2.Blasser-50
+3.Shoe-20
+4.tuck-10
+5.no uniform-200
+FOR GIRL
+1.saree-50
+2.blasser-50
+3.free hair-20
+4.id card-10
+5.shoe'''
